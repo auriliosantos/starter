152d1c541ae53400f709afb5f5b3630113c6ddb7
auriliosantos <auriliosantos@hotmail.com.br>
2020-08-12 16:09:32 -0300

    es6 módulo 3 - exercício 1

--- cursojs/curso-es6/src/main.js
+++ cursojs/curso-es6/src/main.js
@@ -1,4 +1,27 @@
-import ClasseUsuario, { idade as IdadeUsuario } from "./functions";
+// Funão delay aciona o .then após 1s
+const delay = () => new Promise((resolve) => setTimeout(resolve, 1000));
 
-ClasseUsuario.info();
-console.log(IdadeUsuario);
+function umPorSegundo() {
+  delay().then(() => {
+    console.log("1s");
+    delay().then(() => {
+      console.log("2s");
+      delay().then(() => {
+        console.log("3s");
+      });
+    });
+  });
+}
+
+umPorSegundo();
+
+async function umPorSegundo2() {
+  await delay();
+  console.log("1s");
+  await delay();
+  console.log("2s");
+  await delay();
+  console.log("3s");
+}
+
+umPorSegundo2();
