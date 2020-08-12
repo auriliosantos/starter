0fb90e9263e6b94652dbed39c0e4ac79f50575f7
auriliosantos <auriliosantos@hotmail.com.br>
2020-08-12 14:29:52 -0300

    es6 módulo 1 - exercício 4

--- cursojs/curso-es6/bundle.js
+++ cursojs/curso-es6/bundle.js
@@ -1,78 +1,28 @@
 "use strict";
 
-// 3.1
-var arr = [1, 2, 3, 4, 5];
-var traditional1 = arr.map(function (item) {
-  return item + 10;
-});
-var arrow1 = arr.map(function (item) {
-  return item + 10;
-});
-console.log(traditional1);
-console.log(arrow1);
-console.log("---------"); // 3.2
-// Dica: Utilize uma constante pra function
+//primeira parte
+var empresa = {
+  nome: "Rocketseat",
+  endereco: {
+    cidade: "Rio do Sul",
+    estado: "SC"
+  }
+};
+var nome = empresa.nome,
+    _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.log(nome);
+console.log(cidade);
+console.log(estado); //segunda parte
 
-var usuario = {
+function mostraInfo(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  return "".concat(nome, " tem ").concat(idade, " anos.");
+}
+
+console.log(mostraInfo({
   nome: "Diego",
   idade: 23
-};
-
-function mostraIdade(usuario) {
-  return usuario.idade;
-}
-
-var traditional2 = mostraIdade(usuario);
-
-var arrow2 = function arrow2(usuario) {
-  return usuario.idade;
-};
-
-console.log(traditional2);
-console.log(arrow2(usuario));
-console.log("---------"); // 3.3
-// Dica: Utilize uma constante pra function
-
-var nome = "Diego";
-var idade = 23;
-
-function mostraUsuario() {
-  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Diego";
-  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
-  return {
-    nome: nome,
-    idade: idade
-  };
-}
-
-var tradicional31 = mostraUsuario(nome, idade);
-var tradicional32 = mostraUsuario(nome);
-
-var arrow3 = function arrow3() {
-  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Diego";
-  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
-  return {
-    nome: nome,
-    idade: idade
-  };
-};
-
-var arrow31 = arrow3(nome, idade);
-var arrow32 = arrow3(nome);
-console.log(tradicional31);
-console.log(tradicional32);
-console.log(arrow31);
-console.log(arrow32);
-console.log("---------"); // 3.4
-
-var promise = function promise() {
-  return new Promise(function (resolve, reject) {
-    return resolve();
-  });
-};
-
-var arrowPromise = function arrowPromise() {
-  return new Promise(function (resolve, reject) {
-    return resolve();
-  });
-};
+}));
--- cursojs/curso-es6/main.js
+++ cursojs/curso-es6/main.js
@@ -1,60 +1,25 @@
-// 3.1
-const arr = [1, 2, 3, 4, 5];
-
-const traditional1 = arr.map(function (item) {
-  return item + 10;
-});
-
-const arrow1 = arr.map((item) => item + 10);
-
-console.log(traditional1);
-console.log(arrow1);
-console.log("---------");
-
-// 3.2
-// Dica: Utilize uma constante pra function
-const usuario = { nome: "Diego", idade: 23 };
-
-function mostraIdade(usuario) {
-  return usuario.idade;
-}
-
-const traditional2 = mostraIdade(usuario);
-
-const arrow2 = (usuario) => usuario.idade;
-
-console.log(traditional2);
-console.log(arrow2(usuario));
-console.log("---------");
-
-// 3.3
-// Dica: Utilize uma constante pra function
-const nome = "Diego";
-const idade = 23;
-
-function mostraUsuario(nome = "Diego", idade = 18) {
-  return { nome, idade };
-}
-
-const tradicional31 = mostraUsuario(nome, idade);
-const tradicional32 = mostraUsuario(nome);
-
-const arrow3 = (nome = "Diego", idade = 18) => ({ nome, idade });
-
-const arrow31 = arrow3(nome, idade);
-const arrow32 = arrow3(nome);
-
-console.log(tradicional31);
-console.log(tradicional32);
-console.log(arrow31);
-console.log(arrow32);
-console.log("---------");
-
-// 3.4
-const promise = function () {
-  return new Promise(function (resolve, reject) {
-    return resolve();
-  });
+//primeira parte
+const empresa = {
+  nome: "Rocketseat",
+  endereco: {
+    cidade: "Rio do Sul",
+    estado: "SC",
+  },
 };
 
-const arrowPromise = () => new Promise((resolve, reject) => resolve());
+const {
+  nome,
+  endereco: { cidade, estado },
+} = empresa;
+
+console.log(nome);
+console.log(cidade);
+console.log(estado);
+
+//segunda parte
+
+function mostraInfo({ nome, idade }) {
+  return `${nome} tem ${idade} anos.`;
+}
+
+console.log(mostraInfo({ nome: "Diego", idade: 23 }));
