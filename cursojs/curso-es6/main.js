39970120bc93eb92aff7d00f69fa2af1328808aa
auriliosantos <auriliosantos@hotmail.com.br>
2020-08-12 14:37:51 -0300

    es6 módulo 1 - exercício 6

--- cursojs/curso-es6/main.js
+++ cursojs/curso-es6/main.js
@@ -1,32 +1,4 @@
-//primeira parte - REST
-const arr = [1, 2, 3, 4, 5, 6];
-const [x, ...y] = arr;
+const usuario = "Diego";
+const idade = 23;
 
-console.log(x);
-console.log(y);
-
-//segunda parte - REST
-function soma(...itens) {
-  return itens.reduce((total, next) => total + next);
-}
-
-console.log(soma(1, 2, 3, 4, 5, 6)); // 21
-console.log(soma(1, 2)); // 3
-
-// SPREAD
-
-const usuario = {
-  nome: "Diego",
-  idade: 23,
-  endereco: {
-    cidade: "Rio do Sul",
-    uf: "SC",
-    pais: "Brasil",
-  },
-};
-
-const usuario2 = { ...usuario, nome: "Gabriel" };
-const usuario3 = { ...usuario, endereco: { cidade: "Lontras" } };
-
-console.log(usuario2);
-console.log(usuario3);
+console.log(`O usuário ${usuario} possui ${idade} anos`);
