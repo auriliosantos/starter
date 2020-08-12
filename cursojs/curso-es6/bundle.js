a11ba74c78148f5c72331472e5aae2bfe3793d6b
auriliosantos <auriliosantos@hotmail.com.br>
2020-08-12 09:03:32 -0300

    es6 módulo 1 - exercício 3

--- cursojs/curso-es6/bundle.js
+++ cursojs/curso-es6/bundle.js
@@ -1,42 +1,78 @@
 "use strict";
 
-var usuarios = [{
-  nome: "Diego",
-  idade: 23,
-  empresa: "Rocketseat"
-}, {
-  nome: "Gabriel",
-  idade: 15,
-  empresa: "Rocketseat"
-}, {
-  nome: "Lucas",
-  idade: 30,
-  empresa: "Facebook"
-}]; //map
-
-var idade = usuarios.map(function (object) {
-  return object.idade;
-}); //filter
-
-var filtered = usuarios.filter(function (_ref) {
-  var idade = _ref.idade,
-      empresa = _ref.empresa;
-  return idade > 18 && empresa === "Rocketseat";
-}); //find
-
-var found = usuarios.find(function (_ref2) {
-  var empresa = _ref2.empresa;
-  return empresa === "Google";
-}); //várias operações
-
-var multipleOperations = usuarios.map(function (usuario) {
-  usuario.idade *= 2;
-  return usuario;
-}).filter(function (_ref3) {
-  var idade = _ref3.idade;
-  return idade < 50;
+// 3.1
+var arr = [1, 2, 3, 4, 5];
+var traditional1 = arr.map(function (item) {
+  return item + 10;
 });
-console.log(idade);
-console.log(filtered);
-console.log(found);
-console.log(multipleOperations);
+var arrow1 = arr.map(function (item) {
+  return item + 10;
+});
+console.log(traditional1);
+console.log(arrow1);
+console.log("---------"); // 3.2
+// Dica: Utilize uma constante pra function
+
+var usuario = {
+  nome: "Diego",
+  idade: 23
+};
+
+function mostraIdade(usuario) {
+  return usuario.idade;
+}
+
+var traditional2 = mostraIdade(usuario);
+
+var arrow2 = function arrow2(usuario) {
+  return usuario.idade;
+};
+
+console.log(traditional2);
+console.log(arrow2(usuario));
+console.log("---------"); // 3.3
+// Dica: Utilize uma constante pra function
+
+var nome = "Diego";
+var idade = 23;
+
+function mostraUsuario() {
+  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Diego";
+  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
+  return {
+    nome: nome,
+    idade: idade
+  };
+}
+
+var tradicional31 = mostraUsuario(nome, idade);
+var tradicional32 = mostraUsuario(nome);
+
+var arrow3 = function arrow3() {
+  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Diego";
+  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
+  return {
+    nome: nome,
+    idade: idade
+  };
+};
+
+var arrow31 = arrow3(nome, idade);
+var arrow32 = arrow3(nome);
+console.log(tradicional31);
+console.log(tradicional32);
+console.log(arrow31);
+console.log(arrow32);
+console.log("---------"); // 3.4
+
+var promise = function promise() {
+  return new Promise(function (resolve, reject) {
+    return resolve();
+  });
+};
+
+var arrowPromise = function arrowPromise() {
+  return new Promise(function (resolve, reject) {
+    return resolve();
+  });
+};
--- cursojs/curso-es6/main.js
+++ cursojs/curso-es6/main.js
@@ -1,26 +1,60 @@
-const usuarios = [
-  { nome: "Diego", idade: 23, empresa: "Rocketseat" },
-  { nome: "Gabriel", idade: 15, empresa: "Rocketseat" },
-  { nome: "Lucas", idade: 30, empresa: "Facebook" },
-];
+// 3.1
+const arr = [1, 2, 3, 4, 5];
 
-//map
-const idade = usuarios.map((object) => object.idade);
-//filter
-const filtered = usuarios.filter(
-  ({ idade, empresa }) => idade > 18 && empresa === "Rocketseat"
-);
-//find
-const found = usuarios.find(({ empresa }) => empresa === "Google");
-//várias operações
-const multipleOperations = usuarios
-  .map((usuario) => {
-    usuario.idade *= 2;
-    return usuario;
-  })
-  .filter(({ idade }) => idade < 50);
+const traditional1 = arr.map(function (item) {
+  return item + 10;
+});
 
-console.log(idade);
-console.log(filtered);
-console.log(found);
-console.log(multipleOperations);
+const arrow1 = arr.map((item) => item + 10);
+
+console.log(traditional1);
+console.log(arrow1);
+console.log("---------");
+
+// 3.2
+// Dica: Utilize uma constante pra function
+const usuario = { nome: "Diego", idade: 23 };
+
+function mostraIdade(usuario) {
+  return usuario.idade;
+}
+
+const traditional2 = mostraIdade(usuario);
+
+const arrow2 = (usuario) => usuario.idade;
+
+console.log(traditional2);
+console.log(arrow2(usuario));
+console.log("---------");
+
+// 3.3
+// Dica: Utilize uma constante pra function
+const nome = "Diego";
+const idade = 23;
+
+function mostraUsuario(nome = "Diego", idade = 18) {
+  return { nome, idade };
+}
+
+const tradicional31 = mostraUsuario(nome, idade);
+const tradicional32 = mostraUsuario(nome);
+
+const arrow3 = (nome = "Diego", idade = 18) => ({ nome, idade });
+
+const arrow31 = arrow3(nome, idade);
+const arrow32 = arrow3(nome);
+
+console.log(tradicional31);
+console.log(tradicional32);
+console.log(arrow31);
+console.log(arrow32);
+console.log("---------");
+
+// 3.4
+const promise = function () {
+  return new Promise(function (resolve, reject) {
+    return resolve();
+  });
+};
+
+const arrowPromise = () => new Promise((resolve, reject) => resolve());
