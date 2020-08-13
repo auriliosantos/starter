df9434f31fcce97dc4ff116a5b5618411858f4c6
auriliosantos <auriliosantos@hotmail.com.br>
2020-08-13 10:47:34 -0300

    es6 módulo 4 - aplicando conhecimentos anteriores

--- cursojs/curso-es6/src/main.js
+++ cursojs/curso-es6/src/main.js
@@ -1,27 +1,97 @@
-import axios from "axios";
+import api from "./api";
 
-const buscaUsuario = (usuario) => {
-  axios
-    .get(`https://api.github.com/users/${usuario}`)
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((err) => {
-      console.log("Usuário não existe");
-    });
-};
+class App {
+  constructor() {
+    this.repositories = [];
 
-buscaUsuario("diego3g");
+    this.formEl = document.getElementById("repo-form");
+    this.inputEl = document.querySelector("input[name=repository]");
+    this.listEl = document.getElementById("repo-list");
 
-//usando async await
-
-const buscaUsuario2 = async (usuario) => {
-  try {
-    const { data } = await axios.get(`https://api.github.com/users/${usuario}`);
-    console.log(data);
-  } catch (err) {
-    console.log("Usuário não existe");
+    this.registerHandlers();
   }
-};
 
-buscaUsuario2("diego3g");
+  registerHandlers() {
+    this.formEl.onsubmit = (event) => this.addRepository(event);
+  }
+
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement("span");
+      loadingEl.appendChild(document.createTextNode("Carregando"));
+      loadingEl.setAttribute("id", "loading");
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById("loading").remove();
+    }
+  }
+
+  async addRepository(event) {
+    event.preventDefault();
+
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.length === 0) {
+      return;
+    }
+
+    this.setLoading();
+
+    try {
+      const {
+        data: {
+          name,
+          description,
+          html_url,
+          owner: { avatar_url },
+        },
+      } = await api.get(`/repos/${repoInput}`);
+
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url,
+      });
+
+      this.inputEl.value = "";
+
+      this.render();
+    } catch (err) {
+      alert("O repositório não existe");
+    }
+
+    this.setLoading(false);
+  }
+
+  render() {
+    this.listEl.innerHTML = "";
+
+    this.repositories.forEach((repo) => {
+      let imgEl = document.createElement("img");
+      imgEl.setAttribute("src", repo.avatar_url);
+
+      let titleEl = document.createElement("strong");
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement("p");
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement("a");
+      linkEl.setAttribute("target", "_blank");
+      linkEl.setAttribute("href", repo.html_url);
+      linkEl.appendChild(document.createTextNode("Acessar"));
+
+      let listItemEl = document.createElement("li");
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
+  }
+}
+
+new App();
