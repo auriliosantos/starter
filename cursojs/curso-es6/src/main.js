cb98e2e881250acafeb11dd3d20f69cda8ce087f
auriliosantos <auriliosantos@hotmail.com.br>
2020-08-13 08:56:23 -0300

    es6 módulo 3 - exercício 3

--- cursojs/curso-es6/src/main.js
+++ cursojs/curso-es6/src/main.js
@@ -1,29 +1,33 @@
 import axios from "axios";
 
-function getUserFromGithub(user) {
-  axios
-    .get(`https://api.github.com/users/${user}`)
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((err) => {
-      console.log("Usuário não existe");
-    });
-}
-
-getUserFromGithub("diego3g");
-getUserFromGithub("diego3g124123");
-
-//usando async await
-
-async function getUserFromGithub2(user) {
-  try {
-    const { data } = await axios.get(`https://api.github.com/users/${user}`);
-    console.log(data);
-  } catch (err) {
-    console.log("Usuário não existe");
+class Github {
+  static getRepositories(repo) {
+    axios
+      .get(`https://api.github.com/repos/${repo}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log("Repositório não existe");
+      });
   }
 }
 
-getUserFromGithub2("diego3g");
-getUserFromGithub2("diego3g124123");
+Github.getRepositories("rocketseat/unform");
+Github.getRepositories("rocketseat/dslkvmskv");
+
+//usando async await
+
+class Github2 {
+  static async getRepositories(repo) {
+    try {
+      const { data } = await axios.get(`https://api.github.com/repos/${repo}`);
+      console.log(data);
+    } catch (err) {
+      console.log("Repositório não existe");
+    }
+  }
+}
+
+Github2.getRepositories("rocketseat/unform");
+Github2.getRepositories("rocketseat/dslkvmskv");
