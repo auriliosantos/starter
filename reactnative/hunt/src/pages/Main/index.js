1030738845a3a17b4c80ef58232ffbbeba09e055
auriliosantos <auriliosantos@hotmail.com.br>
2020-08-14 14:18:49 -0300

    React native - aula 6

--- reactnative/hunt/src/pages/Main/index.js
+++ reactnative/hunt/src/pages/Main/index.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
+import api from '../../services/api';
+
 export default class Main extends Component {
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+
+    const {docs} = response.data;
+  };
+
   render() {
     return (
       <View>
